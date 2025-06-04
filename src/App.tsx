c889f305d1f228c6ac50bbd83f9984b8c8cf2684
Vladimir <grivle@mail.ru>
2025-06-04 18:07:10 +0700

    add github actions deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,10 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "@/styles/theme.ts";
-// import { grey } from "@mui/material/colors";
-// import {global} from "@/styles/global.ts";
-
-// const inputGlobalStyles = (
-//   <GlobalStyles
-//     styles={{
-//       body: {
-//         // [theme.breakpoints.up("md")]: {
-//         // background: grey[100],
-//         // }
-//         // : theme.palette.primary.main
-//       },
-//     }}
-//   />
-// );
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* {inputGlobalStyles} */}
       <RouterProvider router={router} />
     </ThemeProvider>
   );
