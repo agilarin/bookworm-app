5c41f935f289522ec8eb2cc55dfd50b03cad551b
Vladimir <grivle@mail.ru>
2025-06-04 19:08:08 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
-export default defineConfig(({command}) => ({
-  base: command === 'build' ? "bookworm" : "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "bookworm-app" : "/",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-}))
+}));
